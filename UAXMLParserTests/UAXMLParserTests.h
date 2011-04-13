fc40cfce9ba503d550a78358cda4f360dab2c172
Kristian Trenskow <trenskow@me.com>
2011-04-13 12:15:14 +0200

    Initial Commit

---
+++ UAXMLParserTests/UAXMLParserTests.h
@@ -0,0 +1,17 @@
+//
+//  UAXMLParserTests.h
+//  UAXMLParserTests
+//
+//  Created by Kristian Trenskow on 4/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface UAXMLParserTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ UAXMLParserTests/UAXMLParserTests.m
@@ -0,0 +1,33 @@
+//
+//  UAXMLParserTests.m
+//  UAXMLParserTests
+//
+//  Created by Kristian Trenskow on 4/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "UAXMLParserTests.h"
+
+
+@implementation UAXMLParserTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in UAXMLParserTests");
+}
+
+@end
